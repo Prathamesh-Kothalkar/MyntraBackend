0d9d80bbaff92a8013432e1721c7d2f036071a50
Rishabh <ktyrrishabh99361032@gmail.com>
2024-09-03 15:57:46 +0530

    SetNewThings

--- db.js
+++ db.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose');
+const chalk = require('chalk');
+
 require("dotenv").config();
 const DB_URL=process.env.DB_URL;
 // const DB_URL="mongodb+srv://admin:bvBSmAb6o15AInRw@cluster0.oopo9qg.mongodb.net/myntra-backend";
 mongoose.connect(DB_URL)
-.then(()=>console.log("DB Connection Established"))
-.catch((err)=>{console.log("Error while connecting DB : ",err)});
+.then(()=>console.log(chalk.bgGreen("DB Connection Established")))
+.catch((err)=>{console.log(chalk.bgRed("Error while connecting DB : ",err))});
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const chalk = require('chalk');
 const app = express();
 require("./db")
 const PORT_NO = process.env.PORT_NO||3000;
@@ -10,6 +11,6 @@ app.use(express.json());
 app.use("/api/v1", rootRouter);
 
 app.listen(PORT_NO,()=>{
-    console.log(`Server Running on Port :${PORT_NO}`);
+    console.log(chalk.blue(`Myntra Backend Server Running on Port :${PORT_NO}`));
 });
 
